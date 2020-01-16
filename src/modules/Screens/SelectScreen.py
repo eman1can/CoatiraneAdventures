d260a0dab6cd683af895a63a3b1eb48fca724007
TheEman1can <ethan.wolfe212@gmail.com>
2020-01-16 03:01:25 -0800

    Redo on_size for SelectScreen

--- src/modules/Screens/SelectScreen.py
+++ src/modules/Screens/SelectScreen.py
@@ -16,27 +16,15 @@ class SelectScreen(Screen):
         self.main_screen = main_screen
         self.name = 'select_screen'
 
-        self.title = Label(text="Select a Character!", font_size=self.main_screen.width * .075, size_hint=(None, None), color=(.8, .8, .8, 1), font_name='../res/fnt/Precious.ttf')
-        self.title._label.refresh()
-        self.title.size = self.title._label.texture.size
-        self.title.pos = (self.main_screen.width - self.title.width) / 2, self.main_screen.height * .85 - self.title.height / 2
+        self.title = Label(text="Select a Character!", size_hint=(None, None), color=(.8, .8, .8, 1), font_name='../res/fnt/Precious.ttf')
 
-        button_size = self.main_screen.width * .25, self.main_screen.width * .25
-        button_pos = self.main_screen.width * .5 / 3, (self.main_screen.height - self.main_screen.width * .25) / 2
-        self.newgamebell = HTButton(size=button_size, pos=button_pos, size_hint=(None, None), path='../res/screens/buttons/newgame.bell')
+        self.newgamebell = HTButton(size_hint=(None, None), path='../res/screens/buttons/newgame.bell')
         self.newgamebell.bind(on_touch_down=lambda instance, touch: self.chooseCharacter(instance, touch, 'hero_bell'))
-        self.newgameais = HTButton(size=button_size, pos=(button_pos[0] * 2 + button_size[0], button_pos[1]), size_hint=(None, None), path='../res/screens/buttons/newgame.ais')
+        self.newgameais = HTButton(size_hint=(None, None), path='../res/screens/buttons/newgame.ais')
         self.newgameais.bind(on_touch_down=lambda instance, touch: (self.chooseCharacter(instance, touch, 'badass_ais')))
 
-        self.newgamebell_label = Label(text="Rabbit Foot", font_size=self.main_screen.width * .05, size_hint=(None, None), color=(1, 1, 1, 1), font_name='../res/fnt/Precious.ttf')
-        self.newgamebell_label._label.refresh()
-        self.newgamebell_label.size = self.newgamebell_label._label.texture.size
-        self.newgamebell_label.pos = self.main_screen.width * .5 / 3 * 2 + button_size[0] + (self.main_screen.width * .25 - self.newgamebell_label.width) / 2, (self.main_screen.height - self.main_screen.width * .25) / 2 - self.newgamebell_label.height * 1.75
-
-        self.newgameais_label = Label(text="Battle Princess", font_size=self.main_screen.width * .05, size_hint=(None, None), color=(1, 1, 1, 1), font_name='../res/fnt/Precious.ttf')
-        self.newgameais_label._label.refresh()
-        self.newgameais_label.size = self.newgameais_label._label.texture.size
-        self.newgameais_label.pos = self.main_screen.width * .5 / 3 + (self.main_screen.width * .25 - self.newgameais_label.width) / 2, (self.main_screen.height - self.main_screen.width * .25) / 2 - self.newgameais_label.height * 1.75
+        self.newgamebell_label = Label(text="Rabbit Foot", size_hint=(None, None), color=(1, 1, 1, 1), font_name='../res/fnt/Precious.ttf')
+        self.newgameais_label = Label(text="Battle Princess", size_hint=(None, None), color=(1, 1, 1, 1), font_name='../res/fnt/Precious.ttf')
 
         self.add_widget(self.title)
         self.add_widget(self.newgamebell)
@@ -48,7 +36,6 @@ class SelectScreen(Screen):
     def chooseCharacter(self, instance, touch, choice):
         if instance.collide_point(*touch.pos):
             print('Chosen Character: %s, adding to char Array.' % choice)
-            self.bind(size=self.on_size)
             for x in self.main_screen.characters:
                 if x.get_index() > 2 and x.get_id() != 'enticing_misha':
                     self.main_screen.obtained_characters.append(x.get_index())
@@ -76,8 +63,8 @@ class SelectScreen(Screen):
         if not self.initalized:
             return
         self.title.font_size = self.main_screen.width * .075
-        self.title._label.refresh()
-        self.title.size = self.title._label.texture.size
+        self.title.texture_update()
+        self.title.size = self.title.texture_size
         self.title.pos = (self.main_screen.width - self.title.width) / 2, self.main_screen.height * .85 - self.title.height / 2
 
         button_size = self.main_screen.width * .25, self.main_screen.width * .25
@@ -88,11 +75,11 @@ class SelectScreen(Screen):
         self.newgamebell.pos = button_pos[0] * 2 + button_size[0], button_pos[1]
 
         self.newgamebell_label.font_size = self.main_screen.width * .05
-        self.newgamebell_label._label.refresh()
-        self.newgamebell_label.size = self.newgamebell_label._label.texture.size
+        self.newgamebell_label.texture_update()
+        self.newgamebell_label.size = self.newgamebell_label.texture_size
         self.newgamebell_label.pos = self.main_screen.width * .5 / 3 * 2 + button_size[0] + (self.main_screen.width * .25 - self.newgamebell_label.width) / 2, (self.main_screen.height - self.main_screen.width * .25) / 2 - self.newgamebell_label.height * 1.75
 
         self.newgameais_label.font_size = self.main_screen.width * .05
-        self.newgameais_label._label.refresh()
-        self.newgameais_label.size = self.newgameais_label._label.texture.size
+        self.newgameais_label.texture_update()
+        self.newgameais_label.size = self.newgameais_label.texture_size
         self.newgameais_label.pos = self.main_screen.width * .5 / 3 + (self.main_screen.width * .25 - self.newgameais_label.width) / 2, (self.main_screen.height - self.main_screen.width * .25) / 2 - self.newgameais_label.height * 1.75
