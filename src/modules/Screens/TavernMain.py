8a4f3c71f5fc7944d0f954c9c5d808c434f1ebe1
TheEman1can <ethan.wolfe212@gmail.com>
2020-01-16 05:25:31 -0800

    Redo On_size for TavernMain

--- src/modules/Screens/TavernMain.py
+++ src/modules/Screens/TavernMain.py
@@ -4,43 +4,52 @@ from kivy.uix.label import Label
 from kivy.uix.screenmanager import SlideTransition, RiseInTransition
 from src.modules.HTButton import HTButton
 from src.modules.Screens.RecruitPreview import RecruitPreview
-
 import random
 
+
 class TavernMain(Screen):
-
     def __init__(self, main_screen, **kwargs):
-        self.main_screen = main_screen
+        self.initalized = False
         super().__init__(**kwargs)
-        size = (main_screen.width, main_screen.height)
-        self.bg = Image(size=size, pos=(0, 0), allow_stretch=True, keep_ratio=True,
-                        source='../res/screens/backgrounds/collage.png')
-        self.title = Label(text="[b]Recruitment[/b]", markup=True, font_size=175, color=(1, 1, 1, 1),
-                           pos=(650, main_screen.height - 200), size=(200, 50), size_hint=(None, None))
-        self.recruitButton = HTButton(path='../res/screens/buttons/recruitButton', collide_image="../res/screens/buttons/largebutton.collision.png", size=(270, 180),
-                                      pos=(main_screen.width / 2 - 270 / 2, 100), on_touch_down=self.onRecruit)
-        self.lock = Image(size=size, pos=(0, 0), allow_stretch=True, keep_ratio=True,
-                          source='../res/screens/backgrounds/locked.png')
-        # print("Locking")
-        self.locked = True
-        self.disabled = False
-        self.screen_size = 3840
-        self.back_button = HTButton(size=(256 * size[0] / self.screen_size, 256 * size[0] / self.screen_size), pos=(0, size[1] - (256 * size[0] / self.screen_size)), path='../res/screens/buttons/back', on_touch_up=self.on_back_press)
+        self.name = 'tavern_main'
 
-        self.add_widget(self.bg)
+        self.main_screen = main_screen
+
+        self.unlocked = True
+        self._size = (0, 0)
+
+        self.background = Image(allow_stretch=True, keep_ratio=True, source='../res/screens/backgrounds/collage.png')
+        self.lock = Image(allow_stretch=True, keep_ratio=True, source='../res/screens/backgrounds/locked.png')
+        self.title = Label(text="[b]Recruitment[/b]", markup=True, color=(1, 1, 1, 1), size_hint=(None, None), font_name='../res/fnt/Precious.ttf')
+
+        self.recruit_button = HTButton(path='../res/screens/buttons/recruitButton', size_hint=(None, None), collide_image="../res/screens/buttons/largebutton.collision.png", on_touch_down=self.onRecruit)
+        self.back_button = HTButton(path='../res/screens/buttons/back', size_hint=(None, None), on_touch_down=self.on_back_press)
+
+        self.add_widget(self.background)
         self.add_widget(self.title)
-        self.add_widget(self.recruitButton)
+        self.add_widget(self.recruit_button)
         self.add_widget(self.lock)
         self.add_widget(self.back_button)
-        self.size = size
+        self.initalized = True
 
     def on_size(self, instance, size):
-        self.title.font_size = size[0] / 7.2
-        self.title.pos = size[0] / 3, size[1] / 1.14
-        self.recruitButton.size = size[1] / 4, size[1] / 6
-        self.recruitButton.pos = size[0] / 2 - self.recruitButton.width / 2, size[1] / 10.8
-        self.back_button.size = (256 * size[0] / self.screen_size, 256 * size[0] / self.screen_size)
-        self.back_button.pos = (0, size[1] - self.back_button.height)
+        if not self.initalized or self._size == size:
+            return
+        self._size = size.copy()
+
+        self.background.size = self.size
+        self.lock.size = self.size
+
+        self.title.font_size = self.width * 0.15
+        self.title.texture_update()
+        self.title.size = self.title.texture_size
+        self.title.pos = self.width * 0.1, self.height * 0.95 - self.title.height
+
+        self.recruit_button.size = self.height * 0.15 * 1016 / 716, self.height * 0.15
+        self.recruit_button.pos = self.width * 0.5 - self.recruit_button.width * 0.5, self.height * 0.1
+
+        self.back_button.size = self.width * .05, self.width * .05
+        self.back_button.pos = 0, self.height - self.back_button.height
 
     def reload(self):
         pass
@@ -48,28 +57,18 @@ class TavernMain(Screen):
     def onRecruit(self, instance, touch):
         if instance.collide_point(*touch.pos):
             if self.unlocked:
-                if not self.disabled:
-
-                    # print("Recruit")
-                    notobtained = False
-                    while not notobtained:
-                        index = random.randint(0, len(self.main_screen.characters) - 1)
-                        print(str(index))
-                        print(str(self.main_screen.obtained_characters))
-                        notobtained = index not in self.main_screen.obtained_characters
-                    print(str(self.main_screen.characters[index].get_name()))
-                    preview = RecruitPreview(self.main_screen, self.main_screen.characters[index])
-                    self.main_screen.transition = RiseInTransition(duration=.3)
-                    self.main_screen.display_screen(preview, True, True)
+                notobtained = False
+                while not notobtained:
+                    index = random.randint(0, len(self.main_screen.characters) - 1)
+                    notobtained = index not in self.main_screen.obtained_characters
+                screen = RecruitPreview(self.main_screen, self.main_screen.characters[index], size=self.size)
+                self.main_screen.display_screen(screen, True, True)
 
     def check_unlock(self):
-        print(str(self.main_screen.tavern_unlocked))
         self.unlocked = self.main_screen.tavern_unlocked
         if self.unlocked:
-            print("unlocking")
             self.remove_widget(self.lock)
 
     def on_back_press(self, instance, touch):
         if instance.collide_point(*touch.pos):
-            self.main_screen.transition = SlideTransition()
             self.main_screen.display_screen(None, False, False)
