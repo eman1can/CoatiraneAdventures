2ede0bdad207336b1b6f8e7348e1c8637af49345
TheEman1can <ethan.wolfe212@gmail.com>
2020-01-16 03:00:32 -0800

    Redo Hover/Down Effects for TownScreen Buttons; Update Text on TownScreen; Redo on_size for TownScreen

--- src/modules/Screens/TownScreen.py
+++ src/modules/Screens/TownScreen.py
@@ -1,7 +1,8 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.image import Image
+from kivy.uix.label import Label
 from kivy.properties import NumericProperty
-
+from kivy.graphics import Color, Rectangle
 from src.modules.HTButton import HTButton
 
 class TownScreen(Screen):
@@ -11,33 +12,18 @@ class TownScreen(Screen):
         super(TownScreen, self).__init__(**kwargs)
         self.main_screen = main_screen
         self.name = 'town_screen'
-        self.bgImage = Image(source='../res/screens/backgrounds/townClip.jpg', allow_stretch=True, keep_ratio=True,
-                             size=(main_screen.width, main_screen.height), size_hint=(None, None), pos=(0, 0))
+        self.bgImage = Image(source='../res/screens/backgrounds/townClip.jpg', allow_stretch=True, keep_ratio=True, size_hint=(None, None))
 
-        small_button_size = .1 * main_screen.width, .1 * main_screen.width * 620 / 520
-        large_button_size = .2 * main_screen.width, .2 * main_screen.width * 620 / 920
-
-        current_button_width = .1 * main_screen.width
-        current_button_height = .05 * main_screen.height
-
-        self.tavern_button = HTButton(size=small_button_size, pos=(current_button_width, current_button_height), size_hint=(None, None), path='../res/screens/buttons/TavernButton', collide_image='../res/screens/buttons/smallbutton.collision.png')
+        self.tavern_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/TavernButton', collide_image='../res/screens/buttons/smallbutton.collision.png', border=[0, 0, 0, 0], text="\n\nTaverm", font_name='../res/fnt/Precious.ttf', color=(1, 1, 1, 1))
         self.tavern_button.bind(on_touch_down=self.onTavern)
-        current_button_width += small_button_size[0]
-        current_button_width += .025 * main_screen.width
 
-        self.shopButton = HTButton(size=small_button_size, pos=(current_button_width, current_button_height), size_hint=(None, None), path='../res/screens/buttons/ShopButton', collide_image='../res/screens/buttons/smallbutton.collision.png')
-        current_button_width += small_button_size[0]
-        current_button_width += .025 * main_screen.width
+        self.shopButton = HTButton(size_hint=(None, None), path='../res/screens/buttons/ShopButton', collide_image='../res/screens/buttons/smallbutton.collision.png', border=[0, 0, 0, 0], text="\n\nShop", font_name='../res/fnt/Precious.ttf', color=(1, 1, 1, 1))
 
-        self.craftingButton = HTButton(size=small_button_size, pos=(current_button_width, current_button_height), size_hint=(None, None), path='../res/screens/buttons/CraftingButton', collide_image='../res/screens/buttons/smallbutton.collision.png')
-        current_button_width += small_button_size[0]
-        current_button_width += .025 * main_screen.width
+        self.craftingButton = HTButton(size_hint=(None, None), path='../res/screens/buttons/CraftingButton', collide_image='../res/screens/buttons/smallbutton.collision.png', border=[0, 0, 0, 0], text="\n\nCrafting", font_name='../res/fnt/Precious.ttf', color=(1, 1, 1, 1))
 
-        self.questButton = HTButton(size=small_button_size, pos=(current_button_width, current_button_height), size_hint=(None, None), path='../res/screens/buttons/QuestButton', collide_image='../res/screens/buttons/smallbutton.collision.png')
-        current_button_width += small_button_size[0]
-        current_button_width += .5 * main_screen.width
+        self.questButton = HTButton(size_hint=(None, None), path='../res/screens/buttons/QuestButton', collide_image='../res/screens/buttons/smallbutton.collision.png', border=[0, 0, 0, 0], text="\n\nQuest", font_name='../res/fnt/Precious.ttf', color=(1, 1, 1, 1))
 
-        self.dungeonButton = HTButton(size=large_button_size, pos=(current_button_width, current_button_height), size_hint=(None, None), path='../res/screens/buttons/DungeonButton', collide_image='../res/screens/buttons/largebutton.collision.png')
+        self.dungeonButton = HTButton(size_hint=(None, None), path='../res/screens/buttons/DungeonButton', collide_image='../res/screens/buttons/largebutton.collision.png', border=[0, 0, 0, 0], text="\n\nDungeon", font_name='../res/fnt/Precious.ttf', color=(1, 1, 1, 1))
         self.dungeonButton.bind(on_touch_down=self.onDungeon)
 
         self.add_widget(self.bgImage)
@@ -74,34 +60,39 @@ class TownScreen(Screen):
     def on_size(self, instance, size):
         self.bgImage.size = size
 
-        small_button_size = .1 * self.main_screen.width, .1 * self.main_screen.width * 620 / 520
-        large_button_size = .2 * self.main_screen.width, .2 * self.main_screen.width * 620 / 920
+        small_button_size = .1 * self.main_screen.width, .1 * self.main_screen.width * 754 / 661
+        large_button_size = .2 * self.main_screen.width, .2 * self.main_screen.width * 716 / 1016
 
         current_button_width = .1 * self.main_screen.width
         current_button_height = .05 * self.main_screen.height
 
         self.tavern_button.size = small_button_size
         self.tavern_button.pos = (current_button_width, current_button_height)
+        self.tavern_button.font_size = small_button_size[1] * 0.15
         current_button_width += small_button_size[0]
         current_button_width += .025 * self.main_screen.width
 
         self.shopButton.size = small_button_size
         self.shopButton.pos = (current_button_width, current_button_height)
+        self.shopButton.font_size = small_button_size[1] * 0.15
         current_button_width += small_button_size[0]
         current_button_width += .025 * self.main_screen.width
 
         self.craftingButton.size = small_button_size
         self.craftingButton.pos = (current_button_width, current_button_height)
+        self.craftingButton.font_size = small_button_size[1] * 0.15
         current_button_width += small_button_size[0]
         current_button_width += .025 * self.main_screen.width
 
         self.questButton.size = small_button_size
         self.questButton.pos = (current_button_width, current_button_height)
+        self.questButton.font_size = small_button_size[1] * 0.15
         current_button_width += small_button_size[0]
         current_button_width += .05 * self.main_screen.width
 
         self.dungeonButton.size = large_button_size
         self.dungeonButton.pos = (current_button_width, current_button_height)
+        self.dungeonButton.font_size = large_button_size[1] * 0.1875
 
     def reload(self):
         pass
