be7368cc7239a5861113ba5e17b2458a805aadc0
TheEman1can <ethan.wolfe212@gmail.com>
2020-01-18 19:41:45 -0800

    Redo on_size and update HTButton objects in EmptyCharacterPreview

--- src/modules/Screens/EmptyCharacterPreview.py
+++ src/modules/Screens/EmptyCharacterPreview.py
@@ -1,29 +1,29 @@
+from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.uix.screenmanager import Screen
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-from kivy.uix.label import Label
 from src.modules.HTButton import HTButton
 from kivy.input.providers.wm_touch import WM_MotionEvent
 
 class EmptyCharacterPreviewScreen(Screen):
+    initialized = BooleanProperty(False)
+    main_screen = ObjectProperty(None)
+    preview = ObjectProperty(None)
 
-    def __init__(self, main_screen, preview, size, pos):
-        pos = (0, 0) # Because this is a screen, the pos gets reset to 0, 0
-        self.initalized = False
-        super().__init__(size=size, pos=pos)
-        self.main_screen = main_screen
-        self.preview = preview
-        self._touch = None
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.name = 'empty'
 
-        self.button = HTButton(size=size, pos=pos, size_hint=(None, None), border=[0, 0, 0, 0], path='../res/screens/buttons/empty_overlay', on_touch_down=self.on_button_touch_down, on_touch_up=self.on_button_touch_up)
-        self.add_widget(self.button)
+        self._touch = None
+        self._size = (0, 0)
 
-        self.initalized = True
+        self.button = HTButton(size_hint=(None, None), path='../res/screens/buttons/empty_overlay', on_touch_down=self.on_button_touch_down, on_touch_up=self.on_button_touch_up)
+
+        self.add_widget(self.button)
+        self.initialized = True
 
     def on_size(self, instance, size):
-        if not self.initalized:
+        if not self.initialized or self._size == size:
             return
+        self._size = size.copy()
         self.button.size = size
 
     def is_valid_touch(self, instance, touch):
@@ -42,7 +42,7 @@ class EmptyCharacterPreviewScreen(Screen):
             if self.is_valid_touch(instance, touch):
                 if isinstance(touch, WM_MotionEvent):
                     touch.button = 'left'
-                if touch.button != 'left' or self.preview.isDisabled:
+                if touch.button != 'left' or self.preview.is_disabled:
                     return True
                 self.preview.show_select_screen(self, False)
                 return True
