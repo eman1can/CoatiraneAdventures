cc9bc3aa9bdad908774a09ea3c9b08f3f795e7a0
TheEman1can <ethan.wolfe212@gmail.com>
2020-01-18 19:43:50 -0800

    Update on_size and HTButtons for DungeonMain

--- src/modules/Screens/Dungeon/DungeonMain.py
+++ src/modules/Screens/Dungeon/DungeonMain.py
@@ -1,24 +1,27 @@
-from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, BooleanProperty
+from kivy.uix.screenmanager import Screen
 from kivy.uix.image import Image
 from kivy.uix.label import Label
-from kivy.properties import NumericProperty, StringProperty, BooleanProperty
+
 from src.modules.PartyPortfolio import PartyPortfolio
 from src.modules.HTButton import HTButton
 
+
 class DungeonMain(Screen):
+    initialized = BooleanProperty(False)
+    main_screen = ObjectProperty(None)
     party_score = NumericProperty(0)
     level = NumericProperty(1)
     boss = BooleanProperty(False)
 
-    def __init__(self, main_screen, **kwargs):
-        self.initalized = False
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.name = 'dungeon_main'
 
-        self.main_screen = main_screen
-
         self._size = (0, 0)
+        self._level = -1
+        self._party_score = -1
 
         self.background = Image(source="../res/screens/backgrounds/background.png", allow_stretch=True)
 
@@ -26,11 +29,11 @@ class DungeonMain(Screen):
         self.level_label = Label(text="Level - " + str(self.level), size_hint=(None, None), color=(135 / 255, 28 / 255, 100 / 255, 1), font_name='../res/fnt/Precious.ttf')
         self.party_score_label = Label(text="Party Score - " + str(self.party_score), size_hint=(None, None), color=(24 / 255, 134 / 255, 140 / 255, 1), font_name='../res/fnt/Precious.ttf' )
 
-        self.back_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/back', on_touch_up=self.on_back_press, background_disabled_normal_use=True)
-        self.ascend_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/AscendButton', on_touch_up=self.ascend, background_disabled_normal_use=True)
-        self.descend_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/DescendButton', on_touch_up=self.descend)
+        self.back_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/back', on_release=self.on_back_press)
+        self.ascend_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/AscendButton', on_release=self.ascend)
+        self.descend_button = HTButton(size_hint=(None, None), path='../res/screens/buttons/DescendButton', on_release=self.descend)
 
-        self.portfolio = PartyPortfolio(main_screen, self)
+        self.portfolio = PartyPortfolio(main_screen=self.main_screen, dungeon=self, size_hint=(None, None))
 
         self.add_widget(self.background)
         self.add_widget(self.title)
@@ -40,24 +43,25 @@ class DungeonMain(Screen):
         self.add_widget(self.back_button)
         self.add_widget(self.descend_button)
         self.add_widget(self.ascend_button)
-        self.initalized = True
+        self.initialized = True
 
     def on_enter(self, *args):
         Clock.schedule_interval(lambda dt: self.portfolio.animate_arrows(), 3.5)
         self.update_party_score()
         self.update_buttons()
+        self.reload()
 
     def on_leave(self, *args):
         Clock.unschedule(lambda dt: self.portfolio.animate_arrows())
 
     def reload(self):
-        if self.initalized:
+        if self.initialized:
             if self.portfolio is not None:
                 self.portfolio.reload()
             self.update_party_score()
 
     def on_size(self, instance, size):
-        if not self.initalized or self._size == size:
+        if not self.initialized or self._size == size:
             return
         self._size = size.copy()
 
@@ -67,9 +71,14 @@ class DungeonMain(Screen):
         self.title.pos = self.width - self.title.width - self.width * 0.025, self.height - self.title.height - self.width * 0.025
 
         self.level_label.font_size = self.width * .03
-        self.dispatch('on_level')
+        self.level_label.texture_update()
+        self.level_label.size = self.level_label.texture_size
+        self.level_label.pos = self.width - self.title.width + self.width * 0.1, self.height - self.title.height - self.width * 0.025 - self.level_label.height * 1.5
+
         self.party_score_label.font_size = self.width * .03
-        self.dispatch('on_party_score')
+        self.party_score_label.texture_update()
+        self.party_score_label.size = self.party_score_label.texture_size
+        self.party_score_label.pos = self.width - self.title.width - self.width * 0.025 + self.width * 0.35, self.height - self.title.height - self.width * 0.025 - self.level_label.height * 1.5
 
         self.back_button.size = self.width * .05, self.width * .05
         self.back_button.pos = 0, self.height - self.back_button.height
@@ -89,23 +98,27 @@ class DungeonMain(Screen):
         self.portfolio.pos = self.width * .025, self.height * .025
 
     def on_level(self, instance, level):
-        if not self.initalized:
+        if not self.initialized or self._level == level:
             return
-        self.level_label.text = "Level - " + str(level)
+        self._level = level
+
+        self.level_label.text = "Level - " + str(self.level)
         self.level_label.texture_update()
         self.level_label.size = self.level_label.texture_size
         self.level_label.pos = self.width - self.title.width + self.width * 0.1, self.height - self.title.height - self.width * 0.025 - self.level_label.height * 1.5
 
     def on_party_score(self, instance, party_score):
-        if not self.initalized:
+        if not self.initialized or self._party_score == party_score:
             return
-        self.party_score_label.text = "Party Score - " + str(party_score)
+        self._party_score = party_score
+
+        self.party_score_label.text = "Party Score - " + str(self.party_score)
         self.party_score_label.texture_update()
         self.party_score_label.size = self.party_score_label.texture_size
         self.party_score_label.pos = self.width - self.title.width - self.width * 0.025 + self.width * 0.35, self.height - self.title.height - self.width * 0.025 - self.level_label.height * 1.5
 
     def update_party_score(self):
-        if not self.initalized:
+        if not self.initialized:
             return
         party_score = self.portfolio.get_party_score()
         if self.party_score != party_score:
@@ -121,44 +134,41 @@ class DungeonMain(Screen):
             self.back_button.opacity = 0
             self.ascend_button.disabled = False
 
-    def on_back_press(self, instance, touch):
-        if instance.collide_point(*touch.pos):
-            if not instance.disabled:
-                self.main_screen.display_screen(None, False, False)
+    def on_back_press(self, instance):
+        if not instance.disabled:
+            self.main_screen.display_screen(None, False, False)
 
-    def descend(self, instance, touch):
-        if instance.collide_point(*touch.pos):
-            # print("Delve")
-            self.level += 1
+    def descend(self, instance):
+        # print("Delve")
+        self.level += 1
+        self.update_buttons()
+        # descend = False
+        # for x in self.main_screen.parties[0]:
+        #     if not x == None:
+        #         descend = True
+        # if descend:
+        #     print("Delving into the dungeon")
+        #     # print(str(App.currentparty))
+        #     self.floor = DungeonFloor(self.main_screen, self.level, True, self)
+        #     self.floor.run()
+        # else:
+        #     print("Not enough Characters to explore")
+
+    def ascend(self, instance):
+        if not instance.disabled:
+            self.level -= 1
             self.update_buttons()
-            # descend = False
-            # for x in self.main_screen.parties[0]:
-            #     if not x == None:
-            #         descend = True
-            # if descend:
-            #     print("Delving into the dungeon")
-            #     # print(str(App.currentparty))
-            #     self.floor = DungeonFloor(self.main_screen, self.level, True, self)
-            #     self.floor.run()
+            # # print("Ascend")
+            # if len(shownparty) > 0:
+            #     print("Ascending from dungeon")
+            #     self.level = self.level - 1
+            #     if (self.level < 2):
+            #         print('disabling ascend')
+            #         self.ids['ascend'].disabled = True
+            #         self.ids['ascend_text'].text = '[s]Ascend[/s]'
+            #     else:
+            #         print('enabling ascend')
+            #         self.ids['ascend_text'].text = 'Ascend'
+            #         self.ids['ascend'].disabled = False
             # else:
-            #     print("Not enough Characters to explore")
-
-    def ascend(self, instance, touch):
-        if instance.collide_point(*touch.pos):
-            if not instance.disabled:
-                self.level -= 1
-                self.update_buttons()
-                # # print("Ascend")
-                # if len(shownparty) > 0:
-                #     print("Ascending from dungeon")
-                #     self.level = self.level - 1
-                #     if (self.level < 2):
-                #         print('disabling ascend')
-                #         self.ids['ascend'].disabled = True
-                #         self.ids['ascend_text'].text = '[s]Ascend[/s]'
-                #     else:
-                #         print('enabling ascend')
-                #         self.ids['ascend_text'].text = 'Ascend'
-                #         self.ids['ascend'].disabled = False
-                # else:
-                #     print("not enough Characters to explore")
+            #     print("not enough Characters to explore")
